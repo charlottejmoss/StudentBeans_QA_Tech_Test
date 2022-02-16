b711ceee4228f2b8de3f8d01f17cba861a19bdef
charlottejmoss <charlottejmoss@protonmail.com>
2022-02-16 23:04:10 +0000

    task complete

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -1,6 +1,7 @@
 const ParentPageObject = require('./parentPageObject')
 
 class simpleSearchPageObject extends ParentPageObject {
+  //Given
   goToHomePage () {
     // the below url call is relative to the base url in the wdio.conf.js so the below call will actually just result in going straight to the base url
     browser.url('')
@@ -9,6 +10,34 @@ class simpleSearchPageObject extends ParentPageObject {
   verifyHomePage () {
     this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
   }
+
+  //And
+  // (Accept cookies first)
+  acceptCookies () {
+    const acceptcookies = $('#onetrust-accept-btn-handler')
+    acceptcookies.click()
+
+  }
+
+  openSearchBar () {
+    const searchbar = $('._tf5swf')
+    searchbar.click()
+  }
+
+  verifySearchBar () {
+    this.isElementEqualToExpected($('h3=Recommended For You'), 'Recommended For You')
+  }
+
+  //When
+  enterSamsung () {
+    browser.keys('Samsung')
+  }
+
+  //Then
+  verifySamsung () {
+    this.isElementEqualToExpected($('._63p46ei'), 'Samsung')
+    }
+  
 }
 
 module.exports = simpleSearchPageObject
